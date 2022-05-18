ebb338e7360cad20e25a88e8924e002f851c3fe8
Jas Mann <jaforshort@hotmail.ca>
2022-05-18 20:32:25 +0000

    Added a tail function that returns every value of an array but the head

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Functino Implementation
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+//Test Case
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
