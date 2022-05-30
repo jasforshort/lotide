5459754a4d3eb7c4b2f85b2d922ad73ef6400ad0
Jas Mann <jaforshort@hotmail.ca>
2022-05-30 17:49:04 +0000

    Function that returns a slice of the array with elements taken from the beginning and continues until it finds a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i])
+      return false;
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  // using ternary operator
+  const test = eqArrays(actual, expected);
+  test ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      results.push(element);
+    } else {
+      break;
+    }
+  }
+  return results;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
