c6ff0e10332fea5ab186566b10b8ccf262d966a7
Jas Mann <jaforshort@hotmail.ca>
2022-06-08 19:04:39 +0000

    Refactored and added test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++)
-    if (arr1[i] !== arr2[i])
-      return false;
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   // using ternary operator
@@ -20,5 +12,4 @@ const assertArraysEqual = function(actual, expected) {
   // }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 5]);
+module.exports = assertArraysEqual;
